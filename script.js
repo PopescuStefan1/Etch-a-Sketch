14c6fdf199620844d1cb48733450bfd10cefcef5
Stefan <stefan.ro95@gmail.com>
2022-08-09 19:36:41 +0300

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const container = document.querySelector('#grid-container');
+createGrid(16);
 
-for (i = 0; i < 16 * 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
+function createGrid(size) {
+    const container = document.querySelector('#grid-container');
 
-    div.addEventListener('mouseover', function () { addHighlight(div) });
-    // div.addEventListener('mouseout', function () { removeHighlight(div) });
+    while (container.firstChild) {
+        container.firstChild.remove();
+    }
 
-    container.appendChild(div);
+    const procentage = 100 / size;
+    container.setAttribute('style', `grid-template-columns: repeat(${size}, ${procentage}%);`);
+
+    for (i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+
+        div.addEventListener('mouseover', function () { addHighlight(div) });
+        // div.addEventListener('mouseout', function () { removeHighlight(div) });
+
+        container.appendChild(div);
+    }
+
+    const button = document.querySelector('#squares-button');
+    button.textContent = `${size}x${size}`;
 }
 
 function addHighlight(square) {
@@ -22,5 +36,10 @@ const button = document.querySelector('#squares-button');
 button.addEventListener('click', buttonClick);
 
 function buttonClick() {
-    let squares = prompt("Choose the grid size", "16");
+    let size = 0;
+    while (size < 1 || size > 99) {
+        size = prompt("Choose the grid size (1-99)", "16");
+    }
+
+    createGrid(size);
 }
